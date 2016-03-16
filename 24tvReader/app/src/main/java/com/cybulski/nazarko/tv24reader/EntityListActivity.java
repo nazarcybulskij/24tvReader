4e4a762224f5718f6325d6d599bf7c4924d1b61e
nazar_cybulski_ <nazar.zhybulski@gmail.com>
2016-03-16 22:08:05 +0200

    added library

---
+++ 24tvReader/app/src/main/java/com/cybulski/nazarko/tv24reader/EntityListActivity.java
@@ -0,0 +1,13 @@
+package com.cybulski.nazarko.tv24reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntityListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_entity_list);
+  }
+}
